1d78e8f346486bb10c7213e0f52af8e75b5c8cfe
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-05-14 23:47:03 +0530

    Update mail.php

--- templates/mail.php
+++ templates/mail.php
@@ -1,14 +1,14 @@
 <?php
 if(isset( $_POST['name']))
-$name = $_POST['name'];
+  $name = $_POST['name'];
 if(isset( $_POST['email']))
-$email = $_POST['email'];
+  $email = $_POST['email'];
 if(isset( $_POST['message']))
-$message = $_POST['message'];
+  $message = $_POST['message'];
 if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
+  $subject = $_POST['subject'];
 if(isset( $_POST['phone']))
-$phone = $_POST['phone'];
+  $phone = $_POST['phone'];
 
 $content="From: $name \n Email: $email \n Phone: $phone \n Message: $message";
 $recipient = "mdibrahim1963@gmail.com";
