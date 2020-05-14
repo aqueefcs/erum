5e3ba2383b6892bc7bda3ff326ee884f965b6504
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2020-05-14 15:38:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 	<!-- Google Fonts -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-  <link href="https://fonts.googleapis.com/css2?family=Modak&family=Galada&display=swap" rel="stylesheet"> 
+  	<link href="https://fonts.googleapis.com/css2?family=Modak&family=Galada&display=swap" rel="stylesheet"> 
 	<!-- Bootstrap core CSS -->
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
 	<!-- Material Design Bootstrap -->
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
 
-  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+ 	 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
